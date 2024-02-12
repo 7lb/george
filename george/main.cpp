912538261f85d58d13de183cb4a0a9e3fe62be5d
Luca Benci <benci.luca@tuta.io>
2024-02-12 08:48:48 +0100

    Add george's build.zig
    Also comment out flecs to make sure it builds and runs

--- george/main.cpp
+++ george/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
+//#include <flecs.h>
+
 #include <george/test.hpp>
-#include <flecs.h>
 
 int main()
 {
 	std::cout << "Hello, world! " << GeorgeTest::getFive() << std::endl;
 
-	flecs::world world;
-	auto e = world.entity("Bob");
+	// flecs::world world;
+	// auto e = world.entity("Bob");
 
-	std::cout << "Entity name: " << e.name() << std::endl;
+	// std::cout << "Entity name: " << e.name() << std::endl;
 
 	return 0;
 }
